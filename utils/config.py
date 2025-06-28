b5932b10c71559b81f120a0bbae14d6fd3b2f6ea
Kamil <kamil.albinski@gmail.com>
2025-06-28 14:37:11 +0200

    clean config file

--- utils/config.py
+++ utils/config.py
@@ -1,1 +1,27 @@
-# safe config
+# config.py
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# === Project Root ===
+ROOT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+
+# === Database Configuration ===
+DATABASE_FILE = os.path.join(ROOT_PATH, os.getenv('DATABASE_FILE',''))
+
+# === Folder Paths ===
+MBANK_FOLDER = os.path.join(ROOT_PATH, os.getenv('MBANK_FOLDER',''))
+
+
+PKOTB_FOLDER = os.path.join(ROOT_PATH, os.getenv('PKOTB_FOLDER',''))
+
+# === API Configuration ===
+
+# === Local Passwords ===
+DOCS_WITH_ATTACHMENTS = ['document_template_1','document_template_2']
+pdf_password = os.getenv('PDF_PASSWORD', '')
+
+# === Optional Flags ===
+DEBUG_MODE = os.getenv('DEBUG_MODE', 'False').lower() in ('1', 'true', 'yes')
